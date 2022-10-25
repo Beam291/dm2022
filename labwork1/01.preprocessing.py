017be770ca76ef8541d677564457872ec7c7c577
Beam291 <khoinl291@gmail.com>
2022-10-25 11:17:19 +0700

    read 3 first lines

--- labwork1/01.preprocessing.py
+++ labwork1/01.preprocessing.py
@@ -2,39 +2,18 @@ import itertools
 import json
 import pandas as pd
 
-# data_file = open("../../dataset/Yelp/yelp_academic_dataset_review.json")
-# data = []
-
-# nb_lines = 3
-
-# df = pd.read_json("../../dataset/Yelp/yelp_academic_dataset_review.json", lines=True, chunksize=1)
-# for chunk in df:
-#    print(chunk)
-   
+#open review file
 review_file = open("../../dataset/Yelp/yelp_academic_dataset_review.json")
-for line in itertools.islice(f,3):
+data = []
+
+#Because json file too heavy so only read 3 first lines
+for line in itertools.islice(review_file,3):
   line = line.strip()
   if not line: continue
-  print(json.loads(line))
+  data.append(json.loads(line))
+  
+#DataFrame of review_file
+review_df = pd.DataFrame(data)
 
+#close the review_file
 review_file.close()
-
-# for line in data_file:
-#     data.append(json.loads(line))
-# checkin_df = pd.DataFrame(data)
-
-# #close the data_file
-# data_file.close()
-
-# column_name = list(checkin_df)
-
-# print(column_name)
-
-# N = 3
-# with open("../../dataset/Yelp/yelp_academic_dataset_review.json") as f:
-#     for i in range(0, N):
-#         data.append(json.loads(i))
-#         # print(f.readline(), end = '')
-#     f.close
-# checkin_df = pd.DataFrame(data)
-# print(checkin_df)
