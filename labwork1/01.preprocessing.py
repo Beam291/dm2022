39ad99c299a44e70045e6f7fb644fa2b26e400af
Beam291 <khoinl291@gmail.com>
2022-10-25 14:39:22 +0700

    Punctuation and lowercase

--- labwork1/01.preprocessing.py
+++ labwork1/01.preprocessing.py
@@ -1,22 +1,32 @@
 import itertools
 import json
 import pandas as pd
+import string
 
 #open review file
-review_file = open("../../dataset/Yelp/yelp_academic_dataset_review.json")
+reviewFile = open("../../dataset/Yelp/yelp_academic_dataset_review.json")
 data = []
 
 #Because json file too heavy so only read 3 first lines
-for line in itertools.islice(review_file,3):
+for line in itertools.islice(reviewFile,3):
   line = line.strip()
   if not line: continue
   data.append(json.loads(line))
   
 #DataFrame of review_file
-review_df = pd.DataFrame(data)
+reviewDF = pd.DataFrame(data)
 
 #close the review_file
-review_file.close()
+reviewFile.close()
 
+#Dealing with the first line review of the json file
 #first data
-data1 = review_df.iloc[0]['text']
+reviewText1 = reviewDF.iloc[0]['text']
+
+#remove punctuation
+reviewText1 = reviewText1.translate(str.maketrans('', '', string.punctuation))
+
+#Lowercase
+reviewText1 = reviewText1.lower()
+
+print(reviewText1)
