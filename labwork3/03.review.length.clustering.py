533caf353562a44e3d95ec1ad46c7743f434d361
Beam291 <khoinl291@gmail.com>
2022-10-28 10:04:33 +0700

    Restart

--- labwork3/03.review.length.clustering.py
+++ labwork3/03.review.length.clustering.py
@@ -1,7 +1,31 @@
 import Labwork3Function as func
+import itertools
+import pandas as pd
+import json
 
-reviewDF = func.readFile(10)
-reviewLength = func.reviewLength(reviewDF)
-reviewMatrix = func.reviewMatrix(reviewLength)
-reviewMinumum = func.reviewMinimum(reviewMatrix)
-mergeCluster = func.mergeCluster(reviewMinumum)
+#read file and number of comment you want to load in 
+def readFile(nbComment : int):
+    #open review file
+    reviewFile = open("../dataset/Yelp/yelp_academic_dataset_review.json")
+    data = []
+    #Because json file too heavy so only read 100 first lines
+    for line in itertools.islice(reviewFile, nbComment):
+        line = line.strip()
+        if not line: continue
+        data.append(json.loads(line))
+    #DataFrame of review_file
+    reviewDF = pd.DataFrame(data)
+    #Close the open file
+    reviewFile.close()
+    return reviewDF
+
+def countLength(reviewDF : pd.DataFrame):
+    reviewLength = {}
+    for i in (len(reviewDF)):
+        length = len(reviewDF.iloc[i]['text'])
+        reviewLength[i] = length
+    return reviewLength
+
+#Program start from here
+reviewDF = readFile(10)
+reviewLength = countLength(reviewDF)
--- labwork4/04.review.length.kmeans.py
+++ labwork4/04.review.length.kmeans.py
@@ -1,2 +1,7 @@
 import Labwork4Function as func
 
+reviewDF = func.readFile(10)
+reviewLength = func.reviewLength(reviewDF)
+reviewMatrix = func.reviewMatrix(reviewLength)
+selectRandomPoint = func.randomPoint(reviewLength)
+print(selectRandomPoint)
