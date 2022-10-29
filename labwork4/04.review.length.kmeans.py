9f2365ccc5afbce76eed1d2987e33144a1b6417a
Beam291 <khoinl291@gmail.com>
2022-10-29 11:12:53 +0700

    lab4 remaster

--- labwork4/04.review.length.kmeans.py
+++ labwork4/04.review.length.kmeans.py
@@ -1,5 +1,4 @@
 import itertools
-import random
 import pandas as pd
 import json
 
@@ -8,53 +7,34 @@ def readFile(nbComment : int):
     #open review file
     reviewFile = open("../dataset/Yelp/yelp_academic_dataset_review.json")
     data = []
-    
     #Because json file too heavy so only read 100 first lines
     for line in itertools.islice(reviewFile, nbComment):
         line = line.strip()
         if not line: continue
         data.append(json.loads(line))
-        
     #DataFrame of review_file
     reviewDF = pd.DataFrame(data)
+    #Close the open file
     reviewFile.close()
-    
     return reviewDF
 
-#Find review length
-def reviewLength(reviewDF : pd.DataFrame):
-    reviewLength = []
-    for r in range(reviewDF.shape[0]):
-        length = len(reviewDF.iloc[r]['text'])
-        reviewLength.append(length)
+#count the length of reviews from data
+def countLength(reviewDF : pd.DataFrame):
+    reviewLength = {}
+    for i in range(len(reviewDF)):
+        length = len(reviewDF.iloc[i]['text'])
+        reviewLength[i] = length
     return reviewLength
 
-#create matrix
-def reviewMatrix(reviewLength : list): 
-    distDict = {}
-    for i in reviewLength:
-        distList = []
-        for k in reviewLength:
-            dist2point = abs(k - i)
-            distList.append(dist2point)
-        distDict[reviewLength.index(i)] = distList
-    
-    for key, value in distDict.items():     
-        value : list
-        cluster = {}
-        for v in value:
-            cluster[value.index(v)] = v
-        cluster = {x:y for x,y in cluster.items() if y!=0}
-        distDict[key] = cluster
-    return distDict
-
-#Select 3 random point
-def randomPoint(reviewLength : list):
-    rngPoint = random.sample(reviewLength, 3)
-    return rngPoint
-
-reviewDF = func.readFile(10)
-reviewLength = func.reviewLength(reviewDF)
-reviewMatrix = func.reviewMatrix(reviewLength)
-selectRandomPoint = func.randomPoint(reviewLength)
-print(selectRandomPoint)
+#Calculate the distance between each point then put to a matrix
+def matrixGenerator(reviewLength : dict):
+    distMatrix = {}
+    for key, value in reviewLength.items():
+        dist = {}
+        for key1, value1 in reviewLength.items():
+            dist2point = abs(value - value1)
+            dist[key1] = dist2point
+        #remove 0 from dist
+        dist = {x:y for x,y in dist.items() if y!=0}
+        distMatrix[key] = dist
+    return distMatrix
